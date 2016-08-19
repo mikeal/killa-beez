c065764cc7f907d59449751d84b1510f29659a60
Mikeal Rogers <mikeal.rogers@gmail.com>
2016-08-19 14:58:08 -0700

    Updating signal-exchange.

--- index.js
+++ index.js
@@ -9,8 +9,7 @@ const SimplePeer = require('simple-peer')
 const extend = require('lodash.assign')
 const jsonstream = require('jsonstream2')
 const through2 = require('through2')
-
-const signalExchange = require('../signal-exchange')
+const signalExchange = require('signal-exchange')
 
 const noopCallback = (err) => { if (err) console.error(err) }
 
